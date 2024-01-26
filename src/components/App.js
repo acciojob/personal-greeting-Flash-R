bed152e559c3833aae87fa4b423b083c7fa7217f
Tumwesigye Ronnie <59010748+Flash-R@users.noreply.github.com>
2024-01-26 16:09:31 +0000

    greeting

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React,{ useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [name, setName] = useState("");
+
+  function updateName(e){
+    setName(e.target.value)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input type="text" placeholder="name" onChange={updateName}/>
+        {name!="" ? <p>Hello {name}</p> : "" }
     </div>
   )
 }
